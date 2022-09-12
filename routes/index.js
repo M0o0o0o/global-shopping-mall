613fd8e3b39eb29a3efb6414b8f4723fd4914109
Lee-moo <dlandif22@gmail.com>
2022-09-12 20:59:41 +0900

    Remove hello world api

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express();
 
-router.get("/", (req, res) => {
-  res.status(200).json({ message: "global shopping mall" });
-});
+// router.get("/", (req, res) => {
+//   res.status(200).json({ message: "global shopping mall" });
+// });
 
 module.exports = router;
