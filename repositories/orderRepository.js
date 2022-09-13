3bfd4bd9f2a53c2ec042bbe8e275a7cef190fb07
Lee-moo <dlandif22@gmail.com>
2022-09-13 15:45:10 +0900

    Feat findOrder, findOrders in orderRepository

--- repositories/orderRepository.js
+++ repositories/orderRepository.js
@@ -1,3 +1,4 @@
+const { Op } = require("sequelize");
 const { Order } = require("../database/models");
 const logger = require("../logger");
 
@@ -10,4 +11,36 @@ const createOrder = async (data) => {
   }
 };
 
-module.exports = { createOrder };
+const findOrders = async ({ whereClause, page }) => {
+  try {
+    return await Order.findAndCountAll({
+      attributes: [
+        "order_id",
+        "pay_state",
+        "order_state",
+        "order_state",
+        "buyr_country",
+        "buyr_name",
+      ],
+      where: { [Op.and]: whereClause },
+      order: [["order_date", "DESC"]],
+      offset: (page - 1) * 20,
+      limit: 20,
+    });
+  } catch (err) {
+    console.log(err);
+    logger.error(err);
+    return [];
+  }
+};
+
+const findOrder = async (order_id) => {
+  try {
+    return await Order.findOne({ where: { order_id } });
+  } catch (err) {
+    logger.error(err);
+    return null;
+  }
+};
+
+module.exports = { createOrder, findOrders, findOrder };
