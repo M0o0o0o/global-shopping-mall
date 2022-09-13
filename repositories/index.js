a4849ebf26e11190d2cd4ffd770fdf1ba1dae1d1
Lee-moo <dlandif22@gmail.com>
2022-09-13 12:13:25 +0900

    Feat add countryRepository for creating new order in database

--- repositories/index.js
+++ repositories/index.js
@@ -1,7 +1,10 @@
 const categoryRepository = require("./categoryRepository");
 const couponRepository = require("./couponRepository");
-
+const countryRepository = require("./countryRepository");
+const orderRepository = require("./orderRepository");
 module.exports = {
   categoryRepository,
   couponRepository,
+  countryRepository,
+  orderRepository,
 };
